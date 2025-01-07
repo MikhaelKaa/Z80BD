41235e06c2ca63d5704040d0c4cbcc8a58b34f28
MichailKaa <unionfalse@gmail.com>
2025-01-08 00:10:12 +0300

    fix: 16550 работает нестабильно

--- Proto_on_slot/FW_test/src/main.asm
+++ Proto_on_slot/FW_test/src/main.asm
@@ -12,6 +12,54 @@ uart_16550_port   EQU 0x00ef
     org 0x0000
     ; Запрещаем прерывания.
     di
+
+    org 0x0000
+    ; Запрещаем прерывания.
+    di
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    ld	a, 0x0d
+    ld	bc, 0xfcef
+    out	(c),a
+
+    ld	a, 0x87
+    ld	bc, 0xfaef
+    out	(c),a
+
+    ld	a, 0x83
+    ld	bc, 0xfbef
+    out	(c),a
+
+    ld	a, 0x01
+    ld	bc, 0xf8ef
+    out	(c),a
+
+    ld	a, 0x00
+    ld	bc, 0xf9ef
+    out	(c),a
+
+    ld	a, 0x03
+    ld	bc, 0xfbef
+    out	(c),a
+
+    ld	a, 0x00
+    ld	bc, 0xf9ef
+    out	(c),a
+
+    ld	a, 0x2f
+    ld bc, 0xfcef
+    out	(c),a
+    
 main_loop:
 
     ld a, e
@@ -20,6 +68,18 @@ main_loop:
 
     ld (0x4000), a
     inc e
+
+    ld a, e
+    ld	bc, 0xf8ef
+	out	(c), a
+
+    ld	bc, 0x2ffe
+delay:
+    dec bc
+    ld a, b
+    or c
+    jr nz, delay
+    
     jp main_loop
 
 end:
